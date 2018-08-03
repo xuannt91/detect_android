a9fdc785a8fe2907e88bc38d67d27fdf505c1521
Administrator <xuannt@namviet-corp.com.vn>
2018-08-03 11:06:02 +0700

    fix bug

--- app/src/main/java/com/namviet/detect/MainActivity.java
+++ app/src/main/java/com/namviet/detect/MainActivity.java
@@ -2,22 +2,25 @@ package com.namviet.detect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import namviet.ultils.detect.MobileUtils;
 import namviet.ultils.detect.listener.DetectListener;
 
-public class MainActivity extends AppCompatActivity implements DetectListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MobileUtils mobileUtils = new MobileUtils(this,this);
+        MobileUtils mobileUtils = new MobileUtils(this, new DetectListener() {
+            @Override
+            public void onDetectSuccess(String s) {
+                Toast.makeText(MainActivity.this, "" + s, Toast.LENGTH_LONG).show();
+            }
+        });
         mobileUtils.detectMobile();
     }
 
-    @Override
-    public void onDetectSuccess(String mobile) {
 
-    }
 }
